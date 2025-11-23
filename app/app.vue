2a87283f7f7d52860544dc2d5599aa830df1858c
Patrick Cornelißen <patrick.cornelissen@ars.de>
2025-11-23 21:58:24 +0100

    init

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script>
+// Use a static import for server-side compatibility
+import '~/assets/css/main.css'
+</script>
+
+<template>
+
+  <div class="text-primary">
+    <NuxtRouteAnnouncer />
+    hallo
+  </div>
+</template>
---
+++ app/pages/[...slug].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => queryCollection('docs').path(route.path).first())
+</script>
+
+<template>
+  <ContentRenderer :value="page" />
+</template>
