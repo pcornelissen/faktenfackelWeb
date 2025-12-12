b2dd24737d8b1f02b15193dc3f0768f774efe761
Patrick Cornelißen <patrick.cornelissen@ars.de>
2025-12-12 11:41:41 +0100

    Hilfe FAQ - achtung Block

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -15,7 +15,7 @@ await definePageData({
       Weil Transparenz wichtig ist. Nur so kann man die Quellen selbst überprüfen und sich eine eigene Meinung
       bilden.<br>
       Außerdem ist es fair gegenüber den Autoren, die sich die Mühe gemacht haben, die Inhalte zu erstellen und ihre
-      Meinung in den frei zugänglichen Medien kund tun.
+      Meinung in den frei zugänglichen Medien kund tun. Wenn sie nicht wollen, dass es im Internet gelesen wird, sollten die Inhalte in geschützen Gruppen und Bereichen veröffentlicht werden. Die Entscheidung liegt beim Autor.
     </p>
     <h2>Hilfe, das ist Hell, gibt es keinen Darkmode?</h2>
     <p>
@@ -24,7 +24,10 @@ await definePageData({
     <h2>Super Projekt! Kann ich mithelfen?</h2>
     <p>
       Ja, aber es gibt nicht einfach Schreibzugriff für die Seite.<br>
-      Bitte unter <a href="mailto:kontakt@faktenfackel.de">kontakt@faktenfackel.de</a> melden. Wir besprechen dann alles weitere.
+      Bitte unter <a href="mailto:kontakt@faktenfackel.de">kontakt@faktenfackel.de</a> melden. Wir besprechen dann alles weitere.<br>
+      <br>
+      <b>ACHTUNG:</b><br>
+      Bedrohungen, Beleidigungen, etc. werden ggf. veröffentlicht und zur Anzeige gebracht.
     </p>
   </div>
 </template>
