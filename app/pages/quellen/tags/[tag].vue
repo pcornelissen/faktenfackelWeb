c022f119a103d77435d2895d38394895e52a6004
Patrick Cornelißen <patrick.cornelissen@ars.de>
2026-01-09 17:19:13 +0100

    Tag Seite für Quellen

--- app/pages/quellen/tags/[tag].vue
+++ app/pages/quellen/tags/[tag].vue
@@ -1,54 +1,66 @@
 <script setup lang="ts">
 import { capitalize } from '~/utils/stringUtils'
-import PostsList from '~/components/content/PostsList.vue'
+import { useAsyncData } from '#app'
+import { definePageData } from '~/utils/contentUtils'
 
 const route = useRoute()
 
-const category = route.params.category as string
 const tag = route.params.tag as string
 
-const { data: tagInfo }
-  = await
-  useAsyncData(
-    `faktencheck-${category}`,
-    () => {
-      return queryCollection('faktenchecks').path(`/faktenchecks/tags/${tag}/_info`).first()
-    })
-
-const { data: post }
-  = await
-  useAsyncData(
-    `faktencheck-${category}`,
-    () => {
-      return queryCollection('faktenchecks').path(`/faktenchecks/${category}`).first()
-    })
-
-const title = `Faktenchecks im Bereich ${capitalize(category)} zum Schlagwort "${capitalize(tag)}"`
-
-useSeoMeta({
-  title: post.value?.title || title,
-  description: post.value?.description,
+await definePageData({
+  title: 'Quellen mit Schlagwort: ' + capitalize(tag),
+  pageHeading: 'Quellen mit Schlagwort: ' + capitalize(tag),
 })
 
-const { data: list1 } = await useAsyncData(route.path, () => {
-  return queryCollection('faktenchecks')
-    .select('title', 'subtitle', 'path', 'published', 'tags', 'date')
+const { data: sourcesByTagRaw } = await useAsyncData(route.path, () => {
+  return queryCollection('quellen')
+    .where('tags', 'LIKE', '%"' + tag + '"%')
+    .order('name', 'ASC')
     .all()
 })
-const list = list1.value as Post[]
+const sourcesByTag = sourcesByTagRaw.value as Source[]
 
-function filter(list: Post[]) {
-  return list
-    .filter(item => !!item.published && new Date(item.published) <= new Date())
-    .filter(item => item.tags.map((t: string) => t.toLowerCase()).includes(tag.toLowerCase()))
-    .filter(item => item.path.startsWith(`/faktenchecks/${category}/`))
+const { data: sourceLinksByTagRaw } = await useAsyncData('sourceLinksByTag', () => {
+  return queryCollection('quellenlinks')
+    .where('tags', 'LIKE', '%"' + tag + '"%')
+    .order('path', 'ASC')
+    .all()
+})
+const sourceLinksByTag = sourceLinksByTagRaw.value as SourceLink[]
+
+const { data: sourcesByLinksRaw }
+  = (sourceLinksByTag == null || sourceLinksByTag.length == 0)
+    ? { data: { value: [] } }
+    : await useAsyncData('sourcelink-sources', () => {
+        const builder = queryCollection('quellen').orWhere(
+          (query) => {
+            new Set(sourceLinksByTag.map(l => l.path)).forEach(s => query = query.where('path', '=', '/quellen/' + s.split('/')[2]))
+            return query
+          },
+        )
+        return builder
+          .all()
+      })
+const sourcesByLinks = sourcesByLinksRaw.value as Source[]
+
+const sourcesMap = new Map<string, { source: Source, links: SourceLink[] }>()
+
+function findLinks(sourceLinks: SourceLink[], source: Source) {
+  return sourceLinks.filter(l => l.path.startsWith(source.path + '/'))
+}
+
+for (const source of sourcesByLinks as Source[]) {
+  sourcesMap.set(source.path.split('/').pop() || source.path, {
+    source: source,
+    links: findLinks(sourceLinksByTag, source),
+  })
 }
 </script>
 
 <template>
-  <div v-if="list">
+  <div v-if="sourcesByTag">
     <NuxtLink
-      :to="`/faktenchecks/${category}`"
+      to="/quellen"
       style="display: inline-flex;
     vertical-align: middle;"
     >
@@ -56,38 +68,90 @@ function filter(list: Post[]) {
         name="i-lucide:arrow-left"
         style="margin-right: 0.5rem;"
       />
-      Zurück zum Bereich {{ capitalize(category) }}
+      Zurück zur Quellenliste
     </NuxtLink>
 
-    <h1 style="margin-top: 1rem">
-      {{ title }}
-    </h1>
-
-    <ContentRenderer
-      v-if="tagInfo"
-      :value="tagInfo"
-      class="intro"
-    />
-
-    <PostsList
-      :list="filter(list)"
-      :base-path="`/faktenchecks/${category}`"
-    />
-  </div>
-  <div v-else>
-    Diese Seite existiert nicht!<br>
-    <br>
-    <NuxtLink :to="`/faktencheck/${category}`">
-      Zurück zum Bereich {{ capitalize(category) }}
-    </NuxtLink>
+    <div class="flex groups">
+      <div
+        v-if="sourcesByTag.length > 0"
+        class="results ml-2 mr-4"
+      >
+        <h3>Quellen</h3>
+        <ul class="list-disc ml-4">
+          <li
+            v-for="source in sourcesByTag"
+            :key="source.path"
+          >
+            <NuxtLink
+              :to="source.path"
+            >
+              {{ source.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        v-if="sourceLinksByTag.length > 0"
+        class="results ml-2 mr-4"
+      >
+        <h3>Quellenlinks</h3>
+        <ul class="list-disc ml-4">
+          <li
+            v-for="sourceLink in sourcesMap.values()"
+            :key="sourceLink.source.path"
+          >
+            <NuxtLink
+              :to="sourceLink.source.path"
+            >
+              {{ sourceLink.source.name }}
+            </NuxtLink>
+            <br>
+            <ul class="list-['-_'] ml-4">
+              <li
+                v-for="link in sourceLink.links"
+                :key="link.path"
+              >
+                <NuxtLink
+                  :to="link.path"
+                >
+                  {{ link.title }}
+                </NuxtLink>
+                <br>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div
+        v-if="sourceLinksByTag.length + sourcesByTag.length== 0"
+        class="ml-4 mr-4"
+      >
+        <h2>Oh nein!</h2>
+        Ich habe keine Quelle oder keinen Quellenlink mit dem Schlagwort "{{ tag }}" gefunden.
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.intro {
-  margin-bottom: 2rem;
-  padding: 0.5rem;
-  border-radius: 0.3rem;
-  background-color: #eee;
+.results{
+  max-width: 40%;
+  transition: ease all .5s;
+}
+
+.groups{
+  @apply flex-row
+}
+
+@media screen and (max-width: 650px) {
+  .results{
+    max-width: 100%;
+    transition: ease all .5s;
+  }
+  .groups{
+    @apply flex-col;
+    transition: ease all .5s;
+  }
+
 }
 </style>
